94a7b4c40151e1c75c3ca7fab49ebe6967f27c91
Luke Harrison <35813094+lharrison1313@users.noreply.github.com>
2019-11-27 19:05:54 -0500

    added csv and chart export functionality

--- Machine Learning algos/DataProcessor.py
+++ Machine Learning algos/DataProcessor.py
@@ -2,6 +2,7 @@ import csv
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 #function used for finding the minimum of a single sensor
@@ -44,7 +45,7 @@ def parseCsv(csvFile):
 
 
 #returns an array of peak resonance frequencies based on the number of input partitions
-def processData(csvFile,inputPartitions):
+def processData(csvFile,inputPartitions,figOutputFile):
     data = parseCsv(csvFile)
 
     # splitting datalist values into different partitions
@@ -56,27 +57,27 @@ def processData(csvFile,inputPartitions):
             frequencies.append(findMin(data[inputPartitions[x-1]:inputPartitions[x]]))
 
     #plotting all values
-    xvalues=[0 for x in range(len(data))]
-    yvalues=[0 for x in range(len(data))]
+    xvalues=[]
+    yvalues=[]
     i = 0
     for x in data:
-        xvalues[i] = data[i][0]
-        yvalues[i] = data[i][1]
-        i+=1
-    plt.plot(xvalues,yvalues,"black")
+        xvalues.append(data[i][0])
+        yvalues.append(data[i][1])
+        i += 1
+    plt.plot(xvalues, yvalues, "black")
 
     #plotting peak resonance frequencies
     for x in frequencies :
-        plt.plot(x[0],x[1],"ro")
+        plt.plot(x[0], x[1], "ro")
 
     #setting labels
     plt.xlabel("Frequency [GHz]")
     plt.ylabel("S21 [DB]")
-    #plt.savefig()
+    plt.savefig(figOutputFile)
 
     #plotting partitions
-    for xvals in inputPartitions:
-        plt.axvline(x=data[xvals][0])
+    for i in inputPartitions:
+        plt.axvline(x=data[i][0])
     plt.show()
 
     #putting x values in output array
@@ -88,43 +89,47 @@ def processData(csvFile,inputPartitions):
 
 
 #returns deltas and dataset of a folder containing sensor data and a empty sensor file
-def buildResults(datasetFilePath,emptySensorFilePath,partitions):
+def buildResults( datasetName, datasetFilePath, emptySensorFilePath, partitions, figOutputFile):
 
     filelist = os.listdir(datasetFilePath)
     peakFrequencies = []
     deltas = []
     tempPartitions = partitions
+    dataNum = 1
 
     for file in filelist:
-        peakFrequencies.append(processData(datasetFilePath + file,partitions))
+        peakFrequencies.append(processData(datasetFilePath + file, partitions, figOutputFile+"\\"+datasetName+str(dataNum)))
+        dataNum += 1
 
-    mt = processData(emptySensorFilePath,partitions)
+    mt = processData(emptySensorFilePath, partitions, figOutputFile+"\\"+"EmptySensor")
     
     for data in peakFrequencies:
-        deltas.append(np.subtract(np.asarray(mt),np.asarray(data)))
+        deltas.append(np.subtract(np.asarray(mt), np.asarray(data)),)
 
     return peakFrequencies, mt, deltas
 
 
-
-
 defaultPartitions = [1119, 3589, 5740, 7175, 9086]
-print("Please enter folder path for dataset")
-datasetFilePath = input() + "\\"
-print("Please enter name for dataset")
-datasetName = input()
-print("please enter file path for empty sensor data")
-emptySensorFilePath = input()
+dataSetFilePath = input("Please enter folder path for dataset:\n") + "\\"
+dataSetName = input("Please enter name for dataset:\n")
+emptySensorFilePath = input("please enter file path for empty sensor data:\n")
+outputFilePathFigs = input("please enter file path for peak resonance frequencies figures:\n")
+outputFilePathCsv = input("please enter folder path for deltas output csv file:\n")
 
-results = buildResults(datasetFilePath,emptySensorFilePath,defaultPartitions)
-printData(datasetName + " data",results[0])
+results = buildResults(dataSetName, dataSetFilePath, emptySensorFilePath, defaultPartitions, outputFilePathFigs)
+printData(dataSetName + " data", results[0])
 printData("empty sensor", results[1])
-printData(datasetName + " deltas", results[2] )
+printData(dataSetName + " deltas", results[2])
 
+#placing deltas in csv file
+df = pd.DataFrame.from_records(results[2])
+df.to_csv(outputFilePathCsv+"\\"+dataSetName+"Deltas.csv")
 
 #example input        
-#C:\Users\Luke\Documents\DNA_Hybridization\SensorData\Wood
+#C:\Users\Luke\Documents\GitHub\UREP_Cancer_Detection_Array_Microwave_Sensor\SensorData1\Wood
 #wood
-#C:\Users\Luke\Documents\DNA_Hybridization\SensorData\Empty_Sensor\M1(EMPTY).CSV
+#C:\Users\Luke\Documents\GitHub\UREP_Cancer_Detection_Array_Microwave_Sensor\SensorData1\Empty_Sensor\M1 (EMPTY).CSV
+#C:\Users\Luke\Documents\GitHub\UREP_Cancer_Detection_Array_Microwave_Sensor\Charts\WoodFigs
+#C:\Users\Luke\Documents\GitHub\UREP_Cancer_Detection_Array_Microwave_Sensor\results\Deltas
 
     
